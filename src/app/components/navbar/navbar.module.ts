e0b0ccc3c4dbaeef9419a7cb8e6e9ab24f452719
Andres Herrera <lherrera>
2021-11-29 20:18:51 -0500

    navbar

--- src/app/components/navbar/navbar.module.ts
+++ src/app/components/navbar/navbar.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
   ]
 })
 export class NavbarModule { }
