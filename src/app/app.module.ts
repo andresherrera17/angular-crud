80076c698c0664a6d57b7bd6a4ca24428256f08d
Andres Herrera <lherrera>
2021-11-29 19:11:28 -0500

    estructura proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './pages/employess/details/details.component';
+import { EditComponent } from './pages/employess/edit/edit.component';
+import { ListComponent } from './pages/employess/list/list.component';
+import { NewComponent } from './pages/employess/new/new.component';
+import { EmployeesFormComponent } from './components/employees-form/employees-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    EditComponent,
+    ListComponent,
+    NewComponent,
+    EmployeesFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
