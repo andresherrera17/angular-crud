170370aed30d49b01b5a984103a33f2b08edfb88
Andres Herrera <lherrera>
2021-12-01 18:54:57 -0500

    get employees service

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { IEmployee } from "../interfaces/employee.interface";
-import { Firestore, collectionData, collection, DocumentData } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
-  employees$: Observable<DocumentData[]> = new Observable<DocumentData[]>();
+  employees$: Observable<IEmployee[]> = new Observable();
 
+  private employeesCollection: AngularFirestoreCollection<IEmployee>;
 
-  constructor(firestore: Firestore) {
-    const collectionEmployees = collection(firestore, 'employees');
-    this.employees$ = collectionData(collectionEmployees);
+  constructor(private firestore: AngularFirestore) {
+    this.employeesCollection = firestore.collection<IEmployee>('employees');
+    this.getEmployees();
   }
 
+  private getEmployees(): void {
+    this.employees$ = this.employeesCollection.snapshotChanges().pipe(
+      map(actions => actions.map(elem => elem.payload.doc.data() as IEmployee))
+    )
+  }
 }
