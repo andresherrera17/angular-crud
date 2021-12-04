6ba4e1250941974f8ef29220a224807d2eb36c75
Andres Herrera <lherrera>
2021-12-04 09:17:59 -0500

    metodo eliminar en el servicio

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -21,4 +21,15 @@ export class EmployeesService {
       map(actions => actions.map(elem => { return { id: elem.payload.doc.id, ...elem.payload.doc.data() } as IEmployee }))
     )
   }
+
+  onDeleteEmployees(employeeId: string): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const result = await this.employeesCollection.doc(employeeId).delete();
+        resolve(result)
+      } catch (error: any) {
+        reject(error.message)
+      }
+    });
+  }
 }
