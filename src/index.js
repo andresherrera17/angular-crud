48649882d3d203c2bad70fa0abc9b13d6851c63c
Andres Herrera <lherrera>
2021-12-22 12:05:03 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/angular-crud/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
