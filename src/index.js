a0f8ae7cb76b278beb51e1179776a7207f2be6d1
Andres Herrera <lherrera>
2021-12-22 16:34:25 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ app.use(express.static(__dirname+'/dist/angular-crud'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/angular-crud/index.html'));
 });
-
-app.listen(process.env.PORT || 5000);
+var server_port = process.env.PORT || process.env.PORT || 5000;
+var server_host = process.env.HOST || '0.0.0.0';
+app.listen(server_port, server_host);
