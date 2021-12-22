810b934b96bc83a2814d6a2ccbde5abcf2ff77a8
Andres Herrera <lherrera>
2021-12-22 11:52:18 -0500

    enviromen prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBmYzo9BitkP9KzmA0Dt37KR_9kjoH4_RM",
+    authDomain: "angular-crud-7d11e.firebaseapp.com",
+    projectId: "angular-crud-7d11e",
+    storageBucket: "angular-crud-7d11e.appspot.com",
+    messagingSenderId: "343072713790",
+    appId: "1:343072713790:web:c8d70c7301ff2675ea756f",
+    measurementId: "G-H5JTG0R1Z8"
+  }
 };
